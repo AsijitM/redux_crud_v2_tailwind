d78bdd8d3378ae6fcf51bb0152c72b814cf3e91c
Asijit <asijit00@gmail.com>
2022-08-25 17:18:37 +0530

    Added home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { Route, Routes } from "react-router-dom";
-import AddUser from "./features/users/AddUser";
-import EditUser from "./features/users/EditUser";
-import UserList from "./features/users/UserList";
+import { Route, Routes } from 'react-router-dom';
+import AddUser from './features/users/AddUser';
+import EditUser from './features/users/EditUser';
+import UserList from './features/users/UserList';
 
 function App() {
   return (
     <div className="container mx-auto px-2 max-w-5xl pt-10 md:pt-32">
-      <h1 className="text-center font-bold text-2xl text-gray-700">CRUD with redux toolkit</h1>
+      <h1 className="text-center font-bold text-2xl text-gray-700">
+        CRUD with redux toolkit
+      </h1>
       <Routes>
-        <Route path="/" element={<UserList />} />
+        <Route path="/redux_crud_v2_tailwind" element={<UserList />} />
         <Route path="/add-user" element={<AddUser />} />
         <Route path="/edit-user/:id" element={<EditUser />} />
       </Routes>
